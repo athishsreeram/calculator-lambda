d6949e9a34b7cde774a4f4510ea4a42ed1599903
Athishsreeram, Nharayanan <athishsreeram.nharayanan@bmo.com>
2022-09-13 01:51:34 -0400

    aws lambda cdk commit

--- README.md
+++ README.md
@@ -6,9 +6,19 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+
+brew install node
+
+brew install aws
+
+aws configure
+
+npm install -g cdk
+
+'https://aws.plainenglish.io/aws-typescript-cdk-and-lambda-ab7b0e0a9f89
--- lib/calculator-lambda-stack.ts
+++ lib/calculator-lambda-stack.ts
@@ -1,16 +1,35 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
-export class CalculatorLambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CalculatorLambdaStack extends cdk.Stack {
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new apigateway.RestApi(this, 'api', {
+      description: 'example api gateway',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CalculatorLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 👇 create an Output for the API URL
+    new cdk.CfnOutput(this, 'apiUrl', {value: api.url});
+
+    const calculatorLambda = new lambda.Function(this, 'calculator', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'calculator.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../lambda')),
+    });
+
+    // 👇 add a /todos resource
+    const todos = api.root.addResource('calculator');
+
+    // 👇 integrate GET /todos with getTodosLambda
+    todos.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(calculatorLambda, {proxy: true}),
+    );
+
   }
 }
